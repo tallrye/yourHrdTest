a17162b17259982ab53e2f5f796f6e1fda865661
Serhat Uzuncavdar <s_uzuncavdar@hotmail.com>
2015-10-15 16:54:12 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Your HRD Test
+
+This project is an example of workflow management for a lynda.com course.
---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+'Use Strict';
+angular.module('App', ['ionic','ngStorage', 'ngCordova','firebase','ngMessages'])
+.config(function($stateProvider, $urlRouterProvider) {
+$stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'views/login/login.html',
+      controller:'loginController'
+    })
+    .state('forgot', {
+      url: '/forgot',
+      templateUrl: 'views/forgot/forgot.html',
+      controller:'forgotController'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'views/register/register.html',
+      controller:'registerController'
+    })
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/home/home.html',
+      controller:'homeController'
+    })
+    ;
+$urlRouterProvider.otherwise("/login");
+})
+// Changue this for your Firebase App URL.
+.constant('FURL', 'https://asfirebase.firebaseio.com/')
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
